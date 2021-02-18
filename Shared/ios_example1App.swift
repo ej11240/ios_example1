7492c6d8b40ed387ae6522f05a11b2b444fdb32d
ej11240 <hwangej1124@gmail.com>
2021-02-18 23:28:40 +0900

    first settings done

---
+++ Shared/ios_example1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_example1App.swift
+//  Shared
+//
+//  Created by Eugene Hwang on 2021/02/18.
+//
+
+import SwiftUI
+
+@main
+struct ios_example1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
